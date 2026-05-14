004a9f659efa30417628c71143b000ee73f3b562
rebornloki-dev <lydieakokpe431@gmail.com>
2026-05-14 18:30:58 +0100

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+//console.log("haahahha");
+//console.log("niggur");
+
+//window.alert("How are you my niggur");
+//window.alert("I am fine, thank you for asking");
+//document.getElementById("myH1").textContent = "Hello, World";
+//document.getElementById("myP").textContent = "This is a paragraph.";
+// This is a comment
+//let age = 25;
+//let name = "Jerry"
+//let rank = "2nd";
+
+//let firstName = "Jerry";
+//let favoriteFood = "Burger";
+//let email ="jerry@rebornloki.dev"
+
+//console.log(typeof firstName);
+//window.alert (`Hello, ${firstName}!`);
+//window.alert (`Your favorite food is ${favoriteFood}`);
+//window.alert (`Your email is ${email}`);
+let online = true;
+let forSale = false;
+let isStudent = true;
+
+console.log(`Bro is online :${online}`)
+console.log(`Item is for sale :${forSale}`)
+console.log(`Bro is a student :${isStudent}`)
