e9124b78fc172f8656bcca23c708548931f39c14
rebornloki-dev <lydieakokpe431@gmail.com>
2026-05-16 20:27:14 +0100

    added a practice folder for exercises

---
+++ Practice/Jerry Portfolio/index.js
@@ -0,0 +1,8 @@
+let name = "Jerry";
+let age = 12;
+let email = "jerry@rebornloki.dev";
+let favfood = "Burger";
+
+document.getElementById("p1").textContent = `Hi, my name is ${name} and i am ${age} years old.`
+document.getElementById("p2").textContent = `My email is ${email}(ps it doesnt exist lol).`
+document.getElementById("p3").textContent = `My favorite food is ${favfood}.`
