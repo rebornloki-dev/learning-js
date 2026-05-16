c8e51dd56364f1fe9b10fa3c3e034f1306df21ae
rebornloki-dev <lydieakokpe431@gmail.com>
2026-05-16 19:46:37 +0100

    arranged and organsied the repo

---
+++ Introductions/index.js
@@ -0,0 +1,7 @@
+console.log("haahahha");
+console.log("Hello, World!");
+
+window.alert("How are you ");
+window.alert("I am fine, thank you for asking");
+document.getElementById("myH1").textContent = "Hello, World";
+document.getElementById("myP").textContent = "This is a paragraph.";
---
+++ Strings & Variables/index.js
@@ -0,0 +1,20 @@
+
+let age = 25;
+let name = "Jerry"
+let rank = "2nd";
+let firstName = "Jerry";
+let favoriteFood = "Burger";
+let email ="jerry@rebornloki.dev"
+
+console.log(typeof firstName);
+window.alert (`Hello, ${firstName}!`);
+window.alert (`Your favorite food is ${favoriteFood}`);
+window.alert (`Your email is ${email}`);
+
+let online = true;
+let forSale = false;
+let isStudent = true;
+
+console.log(`Bro is online :${online}`)
+console.log(`Item is for sale :${forSale}`)
+console.log(`Bro is a student :${isStudent}`)
